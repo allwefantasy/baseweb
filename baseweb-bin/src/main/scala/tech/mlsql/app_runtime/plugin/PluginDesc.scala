528a70b2a77b77078c5e1b21c4c242ef01ed8d65
WilliamZhu <allwefantasy@gmail.com>
2020-01-26 17:00:13 +0800

    updte

--- baseweb-bin/src/main/scala/tech/mlsql/app_runtime/plugin/PluginDesc.scala
+++ baseweb-bin/src/main/scala/tech/mlsql/app_runtime/plugin/PluginDesc.scala
@@ -1,9 +1,15 @@
 package tech.mlsql.app_runtime.plugin
 
-import tech.mlsql.serviceframework.platform.{Plugin, PluginItem}
+import tech.mlsql.serviceframework.platform.{AppRuntimeStore, Plugin, PluginItem, PluginLoader}
 
 class PluginDesc extends Plugin {
   override def entries: List[PluginItem] = {
     List()
+  }
+  def registerForTest() = {
+    val pluginLoader = PluginLoader(Thread.currentThread().getContextClassLoader, this)
+    entries.foreach { item =>
+      AppRuntimeStore.store.registerAction(item.name, item.clzzName, pluginLoader)
+    }
   }
 }
