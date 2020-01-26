ec993454217a215a56d12b08ba95aa390d5751a9
WilliamZhu <allwefantasy@gmail.com>
2020-01-26 09:08:43 +0800

    wow

--- baseweb-bin/src/main/java/tech/mlsql/app_runtime/plugin/PluginDesc.scala
+++ baseweb-bin/src/main/java/tech/mlsql/app_runtime/plugin/PluginDesc.scala
@@ -2,9 +2,6 @@ package tech.mlsql.app_runtime.plugin
 
 import tech.mlsql.serviceframework.platform.{Plugin, PluginItem}
 
-/**
- * 21/1/2020 WilliamZhu(allwefantasy@gmail.com)
- */
 class PluginDesc extends Plugin {
   override def entries: List[PluginItem] = {
     List()
