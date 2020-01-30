00dfa3b773672833d6a070bf61a5f8c9582d39cc
WilliamZhu <allwefantasy@gmail.com>
2020-01-30 15:46:05 +0800

    update

--- baseweb-bin/src/main/scala/tech/mlsql/app_runtime/plugin/PluginDesc.scala
+++ baseweb-bin/src/main/scala/tech/mlsql/app_runtime/plugin/PluginDesc.scala
@@ -1,11 +1,13 @@
 package tech.mlsql.app_runtime.plugin
 
+import tech.mlsql.app_runtime.example.action.ExampleAction
 import tech.mlsql.serviceframework.platform.{AppRuntimeStore, Plugin, PluginItem, PluginLoader}
 
 class PluginDesc extends Plugin {
   override def entries: List[PluginItem] = {
-    List()
+    List(ExampleAction.plugin)
   }
+
   def registerForTest() = {
     val pluginLoader = PluginLoader(Thread.currentThread().getContextClassLoader, this)
     entries.foreach { item =>
--- baseweb-lib/src/main/scala/tech/mlsql/app_runtime/example/action/ExampleAction.scala
+++ baseweb-lib/src/main/scala/tech/mlsql/app_runtime/example/action/ExampleAction.scala
@@ -1,10 +1,24 @@
 package tech.mlsql.app_runtime.example.action
 
+import tech.mlsql.app_runtime.db.action.BasicActionProxy
+import tech.mlsql.app_runtime.example.PluginDB
 import tech.mlsql.serviceframework.platform.action.CustomAction
+import tech.mlsql.serviceframework.platform.{PluginItem, PluginType}
 
 /**
  * 21/1/2020 WilliamZhu(allwefantasy@gmail.com)
  */
-class ExampleAction extends CustomAction{
+class ExampleAction extends CustomAction {
   override def run(params: Map[String, String]): String = ???
+}
+
+object ExampleAction {
+  def action = "example"
+
+  def plugin = PluginItem(ExampleAction.action,
+    classOf[ExampleAction].getName, PluginType.action, None)
+}
+
+object ExampleActionProxy {
+  lazy val proxy = new BasicActionProxy(PluginDB.plugin_name)
 }
