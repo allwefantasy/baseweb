e955163f54e7bd154907f27c099681a0fbc8505c
WilliamZhu <allwefantasy@gmail.com>
2021-09-18 17:39:59 +0800

    update

--- baseweb-lib/src/main/scala/tech/mlsql/app_runtime/plugin/ExamplePluginDesc.scala
+++ baseweb-lib/src/main/scala/tech/mlsql/app_runtime/plugin/ExamplePluginDesc.scala
@@ -3,8 +3,11 @@ package tech.mlsql.app_runtime.plugin
 import tech.mlsql.app_runtime.example.action.{ExampleAction, HelloWorldAction}
 import tech.mlsql.serviceframework.platform.{Plugin, PluginItem}
 
-class PluginDesc extends Plugin {
+/**
+ * 18/9/2021 WilliamZhu(allwefantasy@gmail.com)
+ */
+class ExamplePluginDesc extends Plugin {
   override def entries: List[PluginItem] = {
-    List(ExampleAction.plugin, HelloWorldAction.plugin)
+    List(ExampleAction.plugin,HelloWorldAction.plugin)
   }
 }
